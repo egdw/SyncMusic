f3e700667143c1a45772ffcbff6b3523ea36b18a
hdy <378759617@qq.com>
2018-07-28 14:13:36 +0800

    添加配置文件接口

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <aplayer
-      :music="music" preload="auto" :list="musiclist" ref="musicplayer" v-if="isGetDataComplete"/>
+      :music="music" preload="auto" :list="musiclist" ref="musicplayer" :class="controllerstyle" v-if="isGetDataComplete"/>
 
+    <audio id="emitaudio" src="static/audio/emit.mp3">
+      </audio>
     <button @click="test">sdasda</button>
+    <div id="HelloDiv"  v-if="firstopen">
+      <div id="agereeButton" @click="agree">
+        <span>开始使用</span>
+        <p>
+          您的配置信息地址为:
+        </p>
+        <h1>{{configurl}}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +30,7 @@ export default {
   methods: {
     //设置监听器
     setEventListener: function() {
-      var self = this
+      var self = this;
       var audio = document.getElementsByTagName("audio")[0];
       console.log(audio);
       //seeked在跳跃操作完成时触发。
@@ -33,7 +44,7 @@ export default {
       //canplay在媒体数据已经有足够的数据（至少播放数帧）可供播放时触发
       audio.addEventListener("canplay", function() {
         console.log("canplay");
-        self.$refs.musicplayer.play()
+        self.$refs.musicplayer.play();
       });
       //ended播放结束时触发。
       audio.addEventListener("ended", function() {
@@ -104,20 +115,30 @@ export default {
     initData: function(arr) {
       this.music = arr[0];
       this.musiclist = arr;
-      this.isGetDataComplete = true
+      this.isGetDataComplete = true;
       var self = this;
-      setTimeout(function () { 
+      //隐藏开屏
+      //防止某些浏览器无法自动播放音乐.取消监听事件
+      document.removeEventListener("touchstart", function() {
+        function audioAutoPlay() {
+          var musicEle0 = document.getElementById("emitaudio");
+          musicEle0.play();
+        }
+        audioAutoPlay();
+      });
+      this.firstopen = false;
+      setTimeout(function() {
         self.player = self.$refs.musicplayer.$children;
         self.setEventListener();
-       },500)
-      
+      }, 500);
     },
     test: function() {
       // console.log(this.getPlayRandom());
       // Aplayer.play()
       // this.player[2]._props.playIndex =
       // addStatusOne("test","id","12312")
-      console.log(this.$refs.musicplayer)
+      this.sendJson();
+      this.getJson();
     },
     //设置切换的音乐index
     setPlayIndex(index) {
@@ -149,6 +170,116 @@ export default {
       var internalShuffle = this.$refs.musicplayer.internalShuffle;
       console.log(internalShuffle);
       return internalShuffle;
+    },
+    //点击同意按钮时,触发无声的音乐和请求数据
+    agree: function() {
+      console.log(this.$route.params.id);
+      //初始化获取传递过来的参数
+      var id = this.$route.params.id;
+      if (id == undefined || id == "") {
+        this.$route.push("/");
+      }
+      this.songid = id;
+      //初始化数据
+      //可以开房了
+      var self = this;
+      axios
+        .get("https://api.imjad.cn/cloudmusic/", {
+          params: {
+            id: this.songid,
+            type: "playlist"
+          }
+        })
+        .then(function(response) {
+          console.log(response.data);
+          var data = response.data;
+          var temp = 0;
+          if (data.code == "200") {
+            //进行数据处理
+            //获取到播放列表
+            var playlist = data.playlist;
+            var tracks = playlist.tracks;
+            var arr = new Array();
+            tracks.forEach(element => {
+              //歌曲标题
+              var name = element.name;
+              //歌曲id
+              var id = element.id;
+              //作家
+              var artist = "未知";
+              if (element.ar[0] != null && element.ar[0].name != null) {
+                artist = element.ar[0].name;
+              }
+              //封面
+              var pic_img = "";
+              if (element.al != null && element.al.picUrl != null) {
+                pic_img = element.al.picUrl;
+              }
+              //播放地址
+              var url =
+                "https://api.imjad.cn/cloudmusic/?type=song&raw=true&id=" + id;
+              arr[temp] = {
+                title: name,
+                artist: artist,
+                src: url,
+                pic: pic_img
+              };
+              temp++;
+            });
+            // console.log(arr);
+
+            self.initData(arr);
+          } else {
+            alert("歌单不存在");
+            self.$route.push("/");
+          }
+        });
+    },
+    //创建Json数据
+    createJson: function() {
+      var self = this;
+      axios
+        .post("https://api.myjson.com/bins", {
+          params: {
+            data:
+              '{"sync":{"listid":"","time":"","index":"","mode":"","status":"","updatetime":"","random":""}}'
+          }
+        })
+        .then(function(response) {
+          //https://api.myjson.com/bins/s7wgy
+          if (response.data != null && response.data.uri != null) {
+            //获取的当前房间的配置信息
+            uri = response.data.uri;
+            self.configurl = uri;
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    //获取返回的json数据
+    getJson: function() {
+      axios
+        .get("https://api.myjson.com/bins/s7wgy", {})
+        .then(function(response) {
+          console.log(response.data);
+        })
+        .catch(function(error) {});
+    },
+    //修改json数据
+    sendJson: function() {
+      var self = this;
+      axios
+        .put("https://api.myjson.com/bins/s7wgy", {
+          params: {
+            data:
+              '{"sync":{"listid":"937224028","time":"","index":"","mode":"","status":"","updatetime":"","random":""}}'
+          }
+        })
+        .then(function(response) {
+          console.log(response.data);
+        })
+        .catch(function(error) {});
     }
   },
   data() {
@@ -156,74 +287,47 @@ export default {
       songid: "",
       musiclist: null,
       music: null,
-      isGetDataComplete:false
+      isGetDataComplete: false,
+      controllerstyle: "not-hidden-controller",
+      firstopen: true,
+      configurl: "https://api.myjson.com/bins/s7wgy"
     };
   },
   mounted() {
-    console.log(this.$route.params.id);
-    //初始化获取传递过来的参数
-    var id = this.$route.params.id;
-    if (id == undefined || id == "") {
-      this.$route.push("/");
-    }
-    this.songid = id;
-    //初始化数据
-    //可以开房了
-    var self = this;
-    axios
-      .get("https://api.imjad.cn/cloudmusic/", {
-        params: {
-          id: this.songid,
-          type: "playlist"
-        }
-      })
-      .then(function(response) {
-        console.log(response.data);
-        var data = response.data;
-        var temp = 0;
-        if (data.code == "200") {
-          //进行数据处理
-          //获取到播放列表
-          var playlist = data.playlist;
-          var tracks = playlist.tracks;
-          var arr = new Array();
-          tracks.forEach(element => {
-            //歌曲标题
-            var name = element.name;
-            //歌曲id
-            var id = element.id;
-            //作家
-            var artist = "未知";
-            if (element.ar[0] != null && element.ar[0].name != null) {
-              artist = element.ar[0].name;
-            }
-            //封面
-            var pic_img = "";
-            if (element.al != null && element.al.picUrl != null) {
-              pic_img = element.al.picUrl;
-            }
-            //播放地址
-            var url =
-              "https://api.imjad.cn/cloudmusic/?type=song&raw=true&id=" + id;
-            arr[temp] = {
-              title: name,
-              artist: artist,
-              src: url,
-              pic: pic_img
-            };
-            temp++;
-          });
-          // console.log(arr);
+    //防止某些浏览器无法自动播放音乐.取消监听事件
+    document.addEventListener("touchstart", function() {
+      function audioAutoPlay() {
+        var musicEle0 = document.getElementById("emitaudio");
+        musicEle0.play();
+      }
+      audioAutoPlay();
+    });
 
-          self.initData(arr);
-        } else {
-          alert("歌单不存在");
-          self.$route.push("/");
-        }
-      })
+    // 创建房间
+    // this.createJson()
   }
 };
 </script>
 
-<style>
+<style lang="stylus">
+#HelloDiv {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: red;
+}
+
+.hidden-controller {
+  .aplayer-controller {
+    visibility: hidden;
+  }
+}
+
+.not-hidden-controller {
+  .aplayer-controller {
+    visibility;
+  }
+}
 </style>
